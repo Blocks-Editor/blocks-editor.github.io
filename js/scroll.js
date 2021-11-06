e78af369738bed27595627b0290062e1f8dbfaad
Nate St. George <natestgeorge@gmail.com>
2021-11-05 22:38:04 -0600

    Improved head page animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,21 @@
 
-const controller = new ScrollMagic.Controller();
+document.addEventListener("DOMContentLoaded", function(event) {
+    const controller = new ScrollMagic.Controller();
 
-new ScrollMagic.Scene({
-    triggerElement: '#slide-1',
-    offset: 300
-})
-    .on("start", () => {
-        document.getElementById("slide-1-xxxx").classList.add('start');
+    new ScrollMagic.Scene({
+        triggerElement: '#slide-1',
+        offset: 300
     })
-    .addTo(controller);
+        .on("start", () => {
+            document.getElementById("slide-1-xxxx").classList.add('start');
+        })
+        .addTo(controller);
 
-// new ScrollMagic.Scene({
-//     triggerElement: "#slide-1",
-//     triggerHook: 0,
-//     duration: 300
-// })
-//     .setTween(new TweenMax.to("#macbook-mockup", 0.5, {css: {transform: 'translate3d(-500px, 0, 0)'}}))
-//     .addTo(controller);
+    new ScrollMagic.Scene({
+        triggerElement: "#slide-1",
+        triggerHook: 0,
+        duration: 300
+    })
+        .setTween(new TweenMax.to("#macbook-mockup", 0.5, {css: {transform: 'translate3d(-500px, 0, 0)'}}))
+        .addTo(controller);
+});
